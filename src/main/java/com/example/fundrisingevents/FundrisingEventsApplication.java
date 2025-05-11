ecd93dbc1b08bca75dc75e74d7c5f1c217456a78
mat3usq <mmogielnicki123@gmail.com>
2025-05-11 20:01:08 +0200

    initial setup spring boot app

---
+++ src/main/java/com/example/fundrisingevents/FundrisingEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundrisingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundrisingEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundrisingEventsApplication.class, args);
+    }
+
+}
